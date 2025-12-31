e1b30044320cd0f59b6c07c98027a27e0daf2117
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-01 00:01:25 +0545

    chore: add DTOS for creating tags

--- DevHabit.Api/DTOs/Tags/CreateTagDto.cs
+++ DevHabit.Api/DTOs/Tags/CreateTagDto.cs
@@ -1,6 +1,7 @@
 namespace DevHabit.Api.DTOs.Tags;
 
-public class CreateTagDto
+public sealed record CreateTagDto
 {
-    
+    public required string Name { get; set; }
+    public string? Description { get; set; }
 }
