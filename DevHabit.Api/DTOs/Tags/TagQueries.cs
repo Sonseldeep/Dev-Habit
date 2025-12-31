dadc84faba97aa5daf2ab7fc7432029c16e66c33
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-01 00:06:31 +0545

    chore: add Tag Queries for projection

--- DevHabit.Api/DTOs/Tags/TagQueries.cs
+++ DevHabit.Api/DTOs/Tags/TagQueries.cs
@@ -1,6 +1,20 @@
+using System.Linq.Expressions;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.Entities;
+
 namespace DevHabit.Api.DTOs.Tags;
 
 public class TagQueries
 {
-    
+    public static Expression<Func<Tag, TagDto>> ProjectToDto()
+    {
+        return t => new TagDto
+        {
+            Id = t.Id,
+            Name = t.Name,
+            Description = t.Description,
+            CreatedAtUtc = t.CreatedAtUtc,
+            UpdatedAtUtc = t.UpdatedAtUtc,
+        };
+    }
 }
