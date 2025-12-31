585cdd2b24d01f5555ae08dc3a24f4478e734de2
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-01 00:04:53 +0545

    chore: add DTO for update tag

--- DevHabit.Api/DTOs/Tags/UpdateTagDto.cs
+++ DevHabit.Api/DTOs/Tags/UpdateTagDto.cs
@@ -1,3 +1,7 @@
 namespace DevHabit.Api.DTOs.Tags;
 
-public record UpdateTagDto();
+public sealed record UpdateTagDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
